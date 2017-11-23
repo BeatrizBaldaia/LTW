279899d3130fcef666b9350f75c81eb1de76ec1b
joao611 <joaofvde@gmail.com>
2017-11-23 17:50:36 +0000

    If logged in, login.php redirects to initial page.

--- login.php
+++ login.php
@@ -2,6 +2,11 @@
   include_once('includes/init.php');
   include_once('database/users.php');
 
+  if(isset($_SESSION['username'])){
+    header('Location: initial_page.php');
+    die;
+  }
+
   include('templates/common/header.php');
   //include('templates/users/user.php');
   include('templates/common/footer.php');
