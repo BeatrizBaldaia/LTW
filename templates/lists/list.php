0ab99b319f76e144b48f756a5bc31b5d8ef15014
joao611 <joaofvde@gmail.com>
2017-11-23 16:17:58 +0000

    Limited main page to 3 lists

--- templates/lists/list.php
+++ templates/lists/list.php
@@ -1,16 +1,21 @@
 <?php   include_once('database/lists.php');?>
 <section id=lists>
-  <?php 
+  <?php
     $lists = getLists($_SESSION['username']);
-    foreach( $lists as $list) { ?>
+    $numLists = count($lists);
+    for ($i = 0; $i < $numLists; $i++) {
+        if ($i >= 3) {
+            break;
+        }
+        $list = $lists[$i]; ?>
     <article class="list">
       <a href="list_page.php?id_list=<?=$list['id']?>"><h1><?=$list['name']?></h1></a>
       <ul>
-      <?php $items = getItems($list['id']); 
+      <?php $items = getItems($list['id']);
         foreach( $items as $item) { ?>
           <li><?=$item['name']?>
           </li>
-               
+
       <?}?>
       </ul>
     </article>
