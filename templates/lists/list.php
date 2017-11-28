4c7d1167f40cf31f56baf9c6e8087edd97d8d632
BeatrizBaldaia <bibibaldaia@hotmail.com>
2017-11-28 17:36:52 +0000

    olaaa

--- templates/lists/list.php
+++ templates/lists/list.php
@@ -1,5 +1,5 @@
 <?php   include_once('database/lists.php');?>
-<section id=lists>
+<section id="lists">
   <?php
     $lists = getLists($_SESSION['username']);
     $numLists = count($lists);
@@ -9,7 +9,7 @@
         }
         $list = $lists[$i]; ?>
     <article class="list">
-      <a href="list_page.php?id_list=<?=$list['id']?>"><h3><?=$list['name']?></h3></a>
+      <a class="title_link" href="list_page.php?id_list=<?=$list['id']?>"><h3><?=$list['name']?></h3></a>
       <ul>
       <?php $items = getItems($list['id']);
         foreach( $items as $item) { ?>
