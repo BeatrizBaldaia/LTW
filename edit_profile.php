7669c52fba95e9ed89b786eba22b94ad286a0bfd
BeatrizBaldaia <bibibaldaia@hotmail.com>
2017-11-28 11:43:49 +0000

    cenas

--- edit_profile.php
+++ edit_profile.php
@@ -4,7 +4,8 @@
 
   $user = getUser($_SESSION['username']);
 
-  include('templates/common/header.php');
+  // include('templates/common/header.php');
+  set_header("after_login");
   include('templates/users/edit_profile.php');
   include('templates/common/footer.php');
 ?>
