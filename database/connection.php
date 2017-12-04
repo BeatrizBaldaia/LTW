46129cc216ccb2b869f548d5381246e0e9ea52ac
joao611 <joaofvde@gmail.com>
2017-12-04 19:36:22 +0000

    Country list and email in register page

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,14 @@
 <?php
+
   $db = new PDO('sqlite:database/list.db');
   $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $dbPRAGMA = $db->prepare('PRAGMA foreign_keys=ON;');
+  $dbPRAGMA->execute();
+
+  $dbCountries = new PDO('sqlite:countries.db');
+  $dbCountries->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $dbCountries->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbPRAGMA = $dbCountries->prepare('PRAGMA foreign_keys=ON;');
   $dbPRAGMA->execute();
 ?>
