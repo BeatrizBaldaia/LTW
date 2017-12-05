75ba1dbd49686f9a12740321f23aa47009217329
Joao611 <joaofvde@gmail.com>
2017-12-05 00:43:08 +0000

    Merged DB's

--- database/connection.php
+++ database/connection.php
@@ -5,10 +5,4 @@
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $dbPRAGMA = $db->prepare('PRAGMA foreign_keys=ON;');
   $dbPRAGMA->execute();
-
-  $dbCountries = new PDO('sqlite:database/countries.db');
-  $dbCountries->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $dbCountries->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $dbPRAGMA = $dbCountries->prepare('PRAGMA foreign_keys=ON;');
-  $dbPRAGMA->execute();
 ?>
--- database/getcountries.php
+++ database/getcountries.php
@@ -1,16 +1,16 @@
 <?php
 
 // TODO: Shouldn't be remaking PDO but wtf how
-  $dbCountries = new PDO('sqlite:countries.db');
-  $dbCountries->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $dbCountries->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $dbPRAGMA = $dbCountries->prepare('PRAGMA foreign_keys=ON;');
+  $db = new PDO('sqlite:list.db');
+  $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbPRAGMA = $db->prepare('PRAGMA foreign_keys=ON;');
   $dbPRAGMA->execute();
 
   $name = $_GET['name'];
 
   // Get the countries that start with $name
-  $stmt = $dbCountries->prepare("SELECT * FROM countries WHERE upper(name) LIKE upper(?) LIMIT 10");
+  $stmt = $db->prepare("SELECT * FROM countries WHERE upper(name) LIKE upper(?) LIMIT 10");
   $stmt->execute(array("$name%"));
   $countries = $stmt->fetchAll();
 
