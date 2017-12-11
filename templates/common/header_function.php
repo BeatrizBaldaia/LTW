902154031860f2aecce168897da2e4174fb0f2b6
BeatrizBaldaia <bibibaldaia@hotmail.com>
2017-12-11 17:34:10 +0000

    ...

--- templates/common/header_function.php
+++ templates/common/header_function.php
@@ -4,7 +4,7 @@
     <head>
       <title>To-Do</title>
       <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="viewport" content="width=device-width, initial-scale=.5">
       <link href="css/common_header_style.css" rel="stylesheet">
       <link href="css/categories_themes.css" rel="stylesheet">
       <link href="css/single_list_layout.css" rel="stylesheet">
